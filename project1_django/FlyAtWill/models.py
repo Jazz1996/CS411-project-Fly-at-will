4ec8ae71a118ffb838cdb74c0a252e3475f053b6
jianzhe sun <sunjazz1996@gmail.com>
2020-11-07 17:17:58 +0800

    second

--- project1_django/FlyAtWill/models.py
+++ project1_django/FlyAtWill/models.py
@@ -2,17 +2,37 @@ from django.db import models
 
 
 # Create your models here.
+class Airport(models.Model):
+    objects = models.Manager()
+    iatacode = models.CharField(db_column='IATACode', primary_key=True, max_length=3)  # Field name made lowercase.
+    city = models.CharField(db_column='City', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    latitude = models.FloatField(db_column='Latitude', blank=True, null=True)  # Field name made lowercase.
+    longitude = models.FloatField(db_column='Longitude', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Airport'
+
+
 class Flight(models.Model):
     objects = models.Manager()
-    deptcity = models.CharField(db_column='DeptCity', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    arrcity = models.CharField(db_column='ArrCity', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    numbers = models.CharField(db_column='Numbers', primary_key=True, max_length=10)  # Field name made lowercase.
-    airlines = models.CharField(db_column='Airlines', max_length=5, blank=True, null=True)  # Field name made lowercase.
-    flightdate = models.DateField(db_column='FlightDate', blank=True, null=True)  # Field name made lowercase.
-    depttime = models.TimeField(db_column='DeptTime', blank=True, null=True)  # Field name made lowercase.
-    arrtime = models.TimeField(db_column='ArrTime', blank=True, null=True)  # Field name made lowercase.
+    flightno = models.CharField(db_column='FlightNo', primary_key=True, max_length=10)  # Field name made lowercase.
+    airlinecode = models.CharField(db_column='AirlineCode', max_length=2, blank=True, null=True)  # Field name made lowercase.
     deptiata = models.CharField(db_column='DeptIATA', max_length=3, blank=True, null=True)  # Field name made lowercase.
     arriata = models.CharField(db_column='ArrIATA', max_length=3, blank=True, null=True)  # Field name made lowercase.
+    depttime = models.TimeField(db_column='DeptTime', blank=True, null=True)  # Field name made lowercase.
+    arrtime = models.TimeField(db_column='ArrTime', blank=True, null=True)  # Field name made lowercase.
+    aircraft = models.CharField(db_column='Aircraft', max_length=20, blank=True, null=True)  # Field name made lowercase.
+    distance = models.IntegerField(db_column='Distance', blank=True, null=True)  # Field name made lowercase.
+    otd = models.CharField(db_column='OTD', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    avgdelay = models.IntegerField(db_column='AvgDelay', blank=True, null=True)  # Field name made lowercase.
+    monday = models.IntegerField(db_column='Monday', blank=True, null=True)  # Field name made lowercase.
+    tuesday = models.IntegerField(db_column='Tuesday', blank=True, null=True)  # Field name made lowercase.
+    wednesday = models.IntegerField(db_column='Wednesday', blank=True, null=True)  # Field name made lowercase.
+    thursday = models.IntegerField(db_column='Thursday', blank=True, null=True)  # Field name made lowercase.
+    friday = models.IntegerField(db_column='Friday', blank=True, null=True)  # Field name made lowercase.
+    saturday = models.IntegerField(db_column='Saturday', blank=True, null=True)  # Field name made lowercase.
+    sunday = models.IntegerField(db_column='Sunday', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
--- project1_django/FlyAtWill/views.py
+++ project1_django/FlyAtWill/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Flight
-
+from .models import Flight, Airport
+from datetime import datetime
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,7 +14,50 @@ def FlyAtWillPage(request):
 def SearchFlight(request):
     depart_airport = request.POST['From']
     arrival_airport = request.POST['To']
+
+    # depart_date and return_date are string type.
     depart_date = request.POST['Depart']
     return_date = request.POST['Return']
-    result = Flight.objects.filter(deptiata=depart_airport)
-    return render(request, 'result.html', {'result': result})
+
+    depart_day_tmp = datetime.strptime(depart_date, '%Y-%m-%d')
+    depart_day = depart_day_tmp.weekday() + 1
+    if return_date:
+        return_day_tmp = datetime.strptime(return_date, '%Y-%m-%d')
+        return_day = return_day_tmp.weekday() + 1
+
+        if return_day == 1:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(monday=1))
+        elif return_day == 2:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriate=depart_airport) & Q(tuesday=1))
+        elif return_day == 3:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(wednesday=1))
+        elif return_day == 4:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(thursday=1))
+        elif return_day == 5:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(friday=1))
+        elif return_day == 6:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(saturday=1))
+        elif return_day == 7:
+            return_result = Flight.objects.filter(Q(deptiata=arrival_airport) & Q(arriata=depart_airport) & Q(sunday=1))
+
+    if depart_day == 1:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(monday=1))
+    elif depart_day == 2:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(tuesday=1))
+    elif depart_day == 3:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(wednesday=1))
+    elif depart_day == 4:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(thursday=1))
+    elif depart_day == 5:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(friday=1))
+    elif depart_day == 6:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(saturday=1))
+    elif depart_day == 7:
+        one_way_result = Flight.objects.filter(Q(deptiata=depart_airport) & Q(arriata=arrival_airport) & Q(sunday=1))
+
+    if return_date:
+        return render(request, 'return_result.html', {'one_way_result': one_way_result, 'return_result': return_result})
+    else:
+        return render(request, 'one_way_result.html', {'one_way_result': one_way_result})
+
+
