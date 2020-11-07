cd39dab4940d00b6ecbd42b58e0b13edc26d9442
haofeixu <yzxuhaofei@gmail.com>
2020-11-07 17:22:04 +0800

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.png';
+import bgimg from './pic2.jpg'
 import './App.css';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import React from 'react';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import 'fontsource-roboto';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,63 +18,73 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const tripType = [
-  {
-    value: 'oneWay',
-    label: 'One Way'
-  },
-  {
-    value: 'rounded',
-    label: 'Round Trip'
-  }
+  { value: 'oneWay', label: 'One Way' },
+  { value: 'rounded',label: 'Round Trip'}
 ];
 
+const packages = [
+  { value: 'CZ', label: '南航随心飞'},
+  { value: 'CA', label: '国航随便飞'},
+  { value: 'MU', label: '东航悠闲飞'}
+];
 
 function App() {
   const classes = useStyles();
-  const [currency, setCurrency] = React.useState('oneWay');
+  const [trip, setTrip] = React.useState('oneWay');
+  const [airlines, setAirlines] = React.useState('CZ');
 
   const handleChange = (event) => {
-    setCurrency(event.target.value);
+    setTrip(event.target.value);
   };
+  const handleChange2 = (event) => {
+    setAirlines(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Fly At Will </p>
-        </header>
+        <div className="bg-box"><img src={bgimg} className="bg_img" /></div>
+        <div className="logo-box"><img src={logo} className="logo_img" /></div>
+        <div className="text-box"><p>Fly At Will Tool</p></div>
+      </header>
+      <div className="form-class">
         <form className={classes.root} noValidate autoComplete="on">
-          <label><TextField id="outlined-basic" label="Departure" name="depart" variant="outlined" /></label>
-          <label><TextField id="outlined-basic" label="Arrival" name="arrival" variant="outlined" /></label>
+          <TextField id="outlined-basic" label="Departure" name="depart" variant="outlined" />
+          <TextField id="outlined-basic" label="Arrival" name="arrival" variant="outlined" />
           <TextField name="out"
                        id="date"
-                       label="Date Outward"
+                       label="Departure Date"
                        type="date"
-                       className={classes.textField}
                        InputLabelProps={{
                        shrink: true,}}/>
           <TextField name="in"
                        id="date"
-                       label="Date Inward"
+                       label="Return Date"
                        type="date"
-                       className={classes.textField}
                        InputLabelProps={{
                        shrink: true,}}/>
+
           <TextField name="trip"
-            id="standard-select-currency"
             select
             label="Trip Type"
-            value={currency}
+            value={trip}
             onChange={handleChange}
           >
-            {tripType.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
+            {tripType.map((option) => (<MenuItem key={option.value} value={option.value}> {option.label} </MenuItem>))}
+          </TextField>
+
+          <TextField name="airline"
+            select
+            label="Package"
+            value={airlines}
+            onChange={handleChange2}
+          >
+            {packages.map((option) => (<MenuItem key={option.value} value={option.value}> {option.label} </MenuItem>))}
           </TextField>
           <br/>
           <Button type="submit" variant="contained" color="primary">Submit</Button>
         </form>
+      </div>
     </div>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
+import PersonList from './PersonList'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <PersonList />
   </React.StrictMode>,
   document.getElementById('root')
 );
